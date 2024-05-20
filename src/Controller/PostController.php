f9c887a0efac1c5bd896e83953bc46a836b2022b
eyaHwess <eyahwess74@gmail.com>
2024-05-20 20:40:03 +0100

    post controller update

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Controller;
+
 use App\Repository\CommentRepository;
 use App\Repository\UserRepository;
 use App\Entity\Post;
 use App\Entity\File;
 use App\Entity\User;
-
 use App\Form\PostType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,6 +20,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 #[Route('/post')]
 class PostController extends AbstractController
 {
@@ -31,10 +32,10 @@ class PostController extends AbstractController
     }
 
     #[Route('/', name: 'app_post_index', methods: ['GET'])]
-    public function index(PostRepository $postRepository, HttpClientInterface $client,SerializerInterface $serializer ): JsonResponse
+    public function index(PostRepository $postRepository, HttpClientInterface $client, SerializerInterface $serializer): JsonResponse
     {
-        try
-            {$data = $postRepository->findAll();
+        try {
+            $data = $postRepository->findAll();
             $response = $serializer->serialize($data, 'json');
             return new JsonResponse($response, 200, [], true);
         } catch (\Exception $e) {
@@ -42,54 +43,8 @@ class PostController extends AbstractController
         }
     }
 
-
-// #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
-// public function new(Request $request): Response
-// {
-//     $post = new Post();
-//     $form = $this->createForm(PostType::class, $post);
-//     $form->handleRequest($request);
-
-//     if ($form->isSubmitted() && $form->isValid()) {
-//         // Handle file upload
-//         $uploadedFiles = $request->files->get('post')['files'];
-//         $isPublic = $form->get('estPublie')->getData();
-//         $selectedUser = $form->get('user')->getData();
-//         $post->setUser($selectedUser);
-//         // Set 'estPublie' property based on checkbox value
-//         $post->setEstPublie($isPublic);
-//         foreach ($uploadedFiles as $uploadedFile) {
-//             // Create a new File entity for each uploaded file
-//             $file = new File();
-//             // Handle file upload logic and set properties of File entity
-//             // For example:
-//             $fileName = md5(uniqid()) . '.' . $uploadedFile->guessExtension();
-//             $uploadedFile->move($this->getParameter('uploads_directory'), $fileName);
-//             $file->setFileName($fileName);
-//             // Add the file to the Post entity
-//             $post->addFile($file);
-
-//             // Persist the new File entity
-//             $this->entityManager->persist($file);
-//         }
-
-//         // Persist the Post entity
-//         $this->entityManager->persist($post);
-//         $this->entityManager->flush();
-
-//         return $this->redirectToRoute('app_post_index');
-//     }
-
-//     return $this->render('post/new.html.twig', [
-//         'post' => $post,
-//         'form' => $form->createView(),
-//     ]);
-// }
-
-
-
-#[Route('/new', name: 'app_post_new', methods: ['POST'])]
-public function new(Request $request, UserRepository $userRepository): JsonResponse
+    #[Route('/new', name: 'app_post_new', methods: ['POST'])]
+    public function new(Request $request, UserRepository $userRepository): JsonResponse
     {
         try {
             $post = new Post();
@@ -102,29 +57,28 @@ public function new(Request $request, UserRepository $userRepository): JsonRespo
                 throw new \Exception('User not found');
             }
             $post->setUser($user);
-            $post->setFile($request->get('file'));
-            // $uploadedFiles = $request->files->get('files');
-            // if ($uploadedFiles) {
-            //     foreach ($uploadedFiles as $uploadedFile) {
-            //         if ($uploadedFile instanceof UploadedFile) {
-            //             $file = new File();
-            //             $fileName = md5(uniqid()) . '.' . $uploadedFile->guessExtension();
 
-            //             // Save to Symfony directory
-            //             $symfonyUploadsDir = $this->getParameter('symfony_uploads_directory');
-            //             $uploadedFile->move($symfonyUploadsDir, $fileName);
+            // Handle file upload
+            $uploadedFile = $request->files->get('file');
+            if ($uploadedFile instanceof UploadedFile) {
+                // Use original filename
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = md5(uniqid()).'_'.preg_replace('/[^a-zA-Z0-9-_\.]/','_', $originalFilename);
+                $fileName = $safeFilename . '.' . $uploadedFile->guessExtension();
 
-            //             // Copy to Angular directory
-            //             $angularAssetsDir = $this->getParameter('angular_assets_directory');
-            //             copy($symfonyUploadsDir . '/' . $fileName, $angularAssetsDir . '/' . $fileName);
+                // Save to Symfony directory
+                $symfonyUploadsDir = $this->getParameter('uploads_directory');
+                $uploadedFile->move($symfonyUploadsDir, $fileName);
 
-            //             $file->setFileName($fileName);
-            //             $post->addFile($file);
-            //             $this->entityManager->persist($file);
-            //         }
-            //     }
-            // }
+                // Copy to Angular directory
+                $angularAssetsDir = $this->getParameter('angular_assets_directory');
+                if (!is_dir($angularAssetsDir)) {
+                    mkdir($angularAssetsDir, 0777, true);
+                }
+                copy($symfonyUploadsDir . '/' . $fileName, $angularAssetsDir . '/' . $fileName);
 
+                $post->setFile($fileName);
+            }
 
             $this->entityManager->persist($post);
             $this->entityManager->flush();
@@ -141,12 +95,12 @@ public function new(Request $request, UserRepository $userRepository): JsonRespo
         try {
             $post = $postRepository->find($id);
             if ($post === null) {
-                return $this->json(['error' => 'Post not found'], Response::HTTP_NOT_FOUND);
+                return new JsonResponse(['error' => 'Post not found'], Response::HTTP_NOT_FOUND);
             }
             $response = $serializer->serialize($post, 'json');
             return new JsonResponse($response, Response::HTTP_OK, [], true);
         } catch (\Exception $e) {
-            return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -158,9 +112,9 @@ public function new(Request $request, UserRepository $userRepository): JsonRespo
             $post->setContenu($data['contenu']);
             $post->setEstPublie($data['estPublie']);
             $entityManager->flush();
-            return $this->json(['status' => 'Post updated'], Response::HTTP_OK);
+            return new JsonResponse(['status' => 'Post updated'], Response::HTTP_OK);
         } catch (\Exception $e) {
-            return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -179,30 +133,4 @@ public function new(Request $request, UserRepository $userRepository): JsonRespo
             return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
-
-    // #[Route('/{postId}/comments', name: 'app_post_comments', methods: ['GET'])]
-    // public function comments(int $postId, CommentRepository $commentRepository): JsonResponse
-    // {
-    //     try {
-    //         $post = $this->getDoctrine()->getRepository(Post::class)->find($postId);
-    //         if (!$post) {
-    //             return new JsonResponse(['error' => 'Post not found'], Response::HTTP_NOT_FOUND);
-    //         }
-
-    //         $comments = $post->getComments();
-
-    //         $data = [];
-    //         foreach ($comments as $comment) {
-    //             $data[] = [
-    //                 'id' => $comment->getId(),
-    //                 'content' => $comment->getContent(),
-    //                 // Add other properties as needed
-    //             ];
-    //         }
-
-    //         return new JsonResponse($data, Response::HTTP_OK);
-    //     } catch (\Exception $e) {
-    //         return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
-    //     }
-    // }
 }
